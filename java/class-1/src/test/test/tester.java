f984d9eceada2a27a31df080f4e718702294addc
nodove <choisimo@gmail.com>
2025-09-11 10:17:26 +0900

    init: with class (java)

---
+++ java/class-1/src/test/test/tester.java
@@ -0,0 +1,14 @@
+package test;
+
+public class tester{
+    public static void main(String... args) {
+        System.out.println("HELLO");
+        System.out.println("Hello" + "HELLLLLLLLLL");
+        System.out.println("2+4");
+        System.out.println(2+4);
+        System.out.println(5.5);
+        System.out.println("HELLO" + 1 + 2);     // type-casting (숫자 -> 문자로 변경됨)
+        System.out.println(1 + 2 + "HELLO");     // 위와 동일함
+        System.out.println();
+    }
+}
