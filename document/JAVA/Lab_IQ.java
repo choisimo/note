771631b1f48e3f111737f45d47adf5c4d9f0bb9c
nodove <choisimo@gmail.com>
2025-09-18 13:39:53 +0900

    temp

---
+++ document/JAVA/Lab_IQ.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Lab_IQ {
+    public static void main(String[] args) 
+    {
+        System.out.println("입력 주세요 : ");        
+        Scanner sc1 = new Scanner(System.in);
+
+        int getVal = sc1.nextInt();
+        
+        sc1.close();
+
+        if (100 <= getVal) {System.out.println("You are smart.");}
+        else { System.out.println("But I still love you."); }
+   } 
+}
+
+/**
+ * 100.1 을 입력 시에는 
+ * java.util.InputMismatchException 
+ * Exception이 발생함.
+ */
