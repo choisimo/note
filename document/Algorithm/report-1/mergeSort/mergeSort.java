445b58f17a6900bb80170f8356e34511dfec8b6f
nodove <nodove@naver.com>
2025-09-22 09:32:15 +0900

    feat(merge-sort): add initial Java implementation for merge sort

---
+++ document/Algorithm/report-1/mergeSort/mergeSort.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class mergeSort{
+	public static void main(String[] args) {
+		Scanner inputNum = new Scanner(System.in);
+		// 원소의 크기 입력
+		int arrSize = inputNum.nextInt();
+		if (arrSize < 1 | arrSize > 10000) {
+			System.
+		}
+
+			
+	}
+}
